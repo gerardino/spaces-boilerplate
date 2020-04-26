6d5a9c777876f0ce2825cd3e6c6241020aadc473
gerardino <gerardo586x@gmail.com>
2020-04-26 23:04:36 +0200

    Sending spaces JSON requests to request resolver and the rest to the Vue.js application.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     omniauth_callbacks: "overrides/omniauth_callbacks",
   }
-  resources :spaces
+  resources :spaces, constraints: lambda { |req| req.format == :json }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :spaces, to: 'home#index'
 
   root to: 'home#index'
 end
