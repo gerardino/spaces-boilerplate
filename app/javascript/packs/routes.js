cdbce9b1810e2cc1446d0958a8d246e8b9ebc61d
gerardino <gerardo586x@gmail.com>
2020-06-22 08:54:41 +0200

    Checking credentials in router

--- app/javascript/packs/routes.js
+++ app/javascript/packs/routes.js
@@ -2,19 +2,39 @@ import Vue from 'vue/dist/vue.esm';
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
+import { Credentials } from './lib/authentication';
+
 import Error500 from './components/errors/500.vue';
 import Error404 from './components/errors/404.vue';
 
 import Spaces from './components/pages/Spaces.vue';
 import Space from './components/pages/Space.vue';
 
+const checkCredentials = () => {
+  const credentials = Credentials.getCredentialsFromStore();
+
+  if (!credentials) {
+    window.location = '/';
+  }
+}
+
 const router = new VueRouter({
   mode: 'history',
   base: '/app',
   // base: `${I18n.prefix}`,
   routes: [
-    { path: '/', component: Spaces, name: 'spaces' },
-    { path: '/spaces/:id', component: Space, name: 'space' },
+    {
+      path: '/',
+      component: Spaces,
+      name: 'spaces',
+      beforeEnter: checkCredentials
+    },
+    {
+      path: '/spaces/:id',
+      component: Space,
+      name: 'space',
+      beforeEnter: checkCredentials
+    },
     { path: '/500', component: Error500 },
     { path: '/404', component: Error404 },
     { path: '*', redirect: '/404' }
